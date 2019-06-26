a3bc2d124a6c0d4012ddfb26894ddd8e21e8bf7b
Adrien Gallouët <angt@users.noreply.github.com>
2019-06-26 14:33:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# hfuck
+h version of brainfuck
+
+```
+$ make hf
+$ ./hf h hhhhhhh hhh hhhhhhh hhhh hhhh hhhhhhh h  \
+hhhhhhh hhh hhh hhh hh hhhhhhhh hhh hhhhhhh hhhh  \
+hhhh hhhh hhhhhhhh hhhhhhhh hhhhhhhh hh hh hh hhh \
+hhhhhhhh hh hhh hhhhh hhh hhh hhh hhhhh hh hhhhh  \
+hhhhh hh hhhhh hhhh hhhh hhhh hhhh hhh hhhhh hhhh \
+h hhhhh hh hh hh hh hh hhhhh hh hhhhh hhhh hhhh   \
+hhhhh hhhh hhh hhhhh
+hello world
+```
---
+++ hf.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <unistd.h>
+
+static
+int c(const char *v)
+{
+    int i = 0;
+    while (v[i] == 'h' || v[i] == '\'') i++;
+    return i;
+}
+
+int main(int n, char **v)
+{
+    static uint8_t x[1 << 16], *p = x;
+    for (int i = 1; i < n; i++) {
+        int l = 1;
+        switch (c(v[i])) {
+            case 1: (*p)++; break;
+            case 2:    p++; break;
+            case 3: (*p)--; break;
+            case 4:    p--; break;
+            case 5: write(1, p, 1); break;
+            case 6:  read(1, p, 1); break;
+            case 7: if (!*p) while (l) { i++; l -= (c(v[i]) == 8) - (c(v[i]) == 7); }; break;
+            case 8: if ( *p) while (l) { i--; l += (c(v[i]) == 8) - (c(v[i]) == 7); }; break;
+            default: return 1;
+        }
+    }
+}
